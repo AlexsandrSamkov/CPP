d8f4365bf8e6ec07de39c1e668866b84e8943355
weambros <weambros@student.21-school.ru>
2021-06-07 09:09:27 +0300

    Begin

---
+++ day00/ex00/source/megaphone.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+int main(int argc, char **argv)
+{
+	size_t len;
+	if (argc == 1)
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *\n";
+	else
+	{
+		
+		for (int i = 1; i < argc; i++)
+		{
+			len = std::strlen(argv[i]);
+			for (size_t j = 0; j < len; j++)
+				std::cout << (char)std::toupper(argv[i][j]);
+		}
+	}
+
+}
